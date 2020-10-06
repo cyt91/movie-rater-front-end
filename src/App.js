e9c5d039cc188a056e2c02e653c9bc68747975e9
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-10-06 10:28:54 +0800

    fetch movies on compDidMount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2']);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/movies', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 42444ee862e5e065edcd497840a445ae8ad4ca2d',
+      },
+    })
+      .then((response) => response.json())
+      .then((resp) => setMovies(resp))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="App">
