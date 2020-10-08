fb58df97ffa403a14a1169b05da399fec8edec78
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-10-08 12:00:12 +0800

    demo fontAwesome

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 export default function MovieDetails(props) {
   const { movie } = props;
   return (
-    <div>
-      <h1>{movie?.title}</h1>
-      <p>{movie?.description}</p>
-    </div>
+    <>
+      {movie ? (
+        <div>
+          <h1>{movie.title}</h1>
+          <p>{movie.description}</p>
+          <FontAwesomeIcon icon={faStar} />
+        </div>
+      ) : (
+        <div />
+      )}
+    </>
   );
 }
