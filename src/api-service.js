ceb6f5b9b51970894ee296d4616da879befbe4c0
AURORA_NT\yewtuck.cheah <yewtuck.cheah@rovisys.com>
2020-10-21 13:10:03 +0800

    set up login API and login UI

--- src/api-service.js
+++ src/api-service.js
@@ -32,4 +32,14 @@ export default class API {
       },
     });
   }
+
+  static loginUser(body) {
+    return fetch(`http://127.0.0.1:8000/auth/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then((response) => response.json());
+  }
 }
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import API from '../api-service';
 
 export default function Auth() {
-  return <h1>Auth</h1>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginClicked = () => {
+    API.loginUser({ username, password })
+      .then((response) => console.log(response.token))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div>
+      <label htmlFor="username">Username</label>
+      <br />
+      <input
+        id="username"
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input
+        id="password"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <br />
+      <button type="submit" onClick={loginClicked}>
+        Login
+      </button>
+    </div>
+  );
 }
